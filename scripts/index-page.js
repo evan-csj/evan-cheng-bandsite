40b7fff1980af4d6b0c932fec3a0ff540589dcc2
Evan Cheng <evancheng2014@gmail.com>
2022-12-13 23:29:52 -0800

    dynamic date

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,31 +2,44 @@ const commentsDiv = document.getElementById("comments");
 const form = document.getElementById("form");
 const photoAddress = "../assets/images/Mohan-muruge.jpg";
 
-let date = new Date();
-let today = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+let date;
+let today;
+let time;
 
 let defaultComment = [
     {
         photo: false,
         name: "Conner Walton",
         date: "02/17/2021",
+        time: "",
+        showDate: "02/17/2021",
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     },
     {
         photo: false,
         name: "Emilie Beach",
         date: "01/09/2021",
+        time: "",
+        showDate: "01/09/2021",
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
         photo: false,
         name: "Miles Acosta",
         date: "12/20/2020",
+        time: "",
+        showDate: "12/20/2020",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
 ];
 
-let displayComment = function (userComment) {
+const getDate = () => {
+    date = new Date();
+    today = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+    time = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+}
+
+const displayComment = function (userComment) {
     let comment = document.createElement("article");
     comment.classList.add("comment");
     let leftDiv = document.createElement("div");
@@ -51,7 +64,7 @@ let displayComment = function (userComment) {
     name.innerText = userComment.name;
     let date = document.createElement("p");
     date.classList.add("comment__date");
-    date.innerText = userComment.date;
+    date.innerText = userComment.showDate;
     upperDiv.appendChild(name);
     upperDiv.appendChild(date);
 
@@ -74,27 +87,44 @@ const refreshComments = () => {
     }
 }
 
-let submitHandle = (event) => {
+const dynamicDate = () => {
+    for (let i = 0; i < defaultComment.length; i++) {
+        if (defaultComment[i].date === today) {
+            let secDiff = time - defaultComment[i].time;
+            let hrDiff = parseInt(secDiff / 3600);
+            let minDiff = parseInt(secDiff / 60);
+
+            if (hrDiff >= 1) {
+                defaultComment[i].showDate = hrDiff + " hours ago";
+            } else if (minDiff >= 1) {
+                defaultComment[i].showDate = minDiff + " minutes ago";
+            } else {
+                defaultComment[i].showDate = secDiff + " seconds ago";
+            }
+        } else {
+            defaultComment[i].showDate = defaultComment[i].date;
+        }
+    }
+}
+
+const submitHandle = (event) => {
     event.preventDefault();
-    // const name = event.target.name.value;
-    // const comment = event.target.comment.value;
+    getDate();
+
     const formData = new FormData(form);
-    // let newComment = {
-    //     photo: true,
-    //     name: name,
-    //     date: "01/01/2022",
-    //     comment: comment
-    // };
 
     let newComment = {
         photo: true,
         name: formData.get("name"),
         date: today,
+        time: time,
+        showDate: "",
         comment: formData.get("comment")
     };
 
     defaultComment.unshift(newComment);
     event.target.reset();
+    dynamicDate();
     refreshComments(commentsDiv);
 }
 
