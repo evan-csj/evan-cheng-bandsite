d05078758529eb4fbfbe6897304221f0d1441347
Evan Cheng <evancheng2014@gmail.com>
2022-12-18 01:16:52 -0800

    dynamic time with dayjs

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -7,42 +7,29 @@ const commentErrorMsg = "Please enter something";
 const photoAddress = "../assets/images/Mohan-muruge.jpg";
 let submitStatus = false;
 
-let date;
-let today;
-let time;
-
 let defaultComment = [
     {
         photo: false,
         name: "Conner Walton",
-        date: "02/17/2021",
-        time: "",
-        showDate: "02/17/2021",
+        date: dayjs("02/17/2021"),
+        show: "",
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     },
     {
         photo: false,
         name: "Emilie Beach",
-        date: "01/09/2021",
-        time: "",
-        showDate: "01/09/2021",
+        date: dayjs("01/09/2021"),
+        show: "",
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
         photo: false,
         name: "Miles Acosta",
-        date: "12/20/2020",
-        time: "",
-        showDate: "12/20/2020",
+        date: dayjs("12/20/2020"),
+        show: "",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     }
 ];
-
-const getDate = () => {
-    date = new Date();
-    today = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-    time = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
-}
 
 const displayComment = function (userComment) {
     let comment = document.createElement("article");
@@ -69,7 +56,7 @@ const displayComment = function (userComment) {
     name.innerText = userComment.name;
     let date = document.createElement("p");
     date.classList.add("comment__date");
-    date.innerText = userComment.showDate;
+    date.innerText = userComment.show;
     upperDiv.appendChild(name);
     upperDiv.appendChild(date);
 
@@ -92,22 +79,36 @@ const refreshComments = () => {
     }
 }
 
-const dynamicDate = () => {
+const dynamicDate = (now) => {
     for (let i = 0; i < defaultComment.length; i++) {
-        if (defaultComment[i].date === today) {
-            let secDiff = time - defaultComment[i].time;
-            let hrDiff = parseInt(secDiff / 3600);
-            let minDiff = parseInt(secDiff / 60);
+        let monthDiff = now.diff(defaultComment[i].date, "month", true);
+        let dayDiff = now.diff(defaultComment[i].date, "day", true);
+        let hrDiff = now.diff(defaultComment[i].date, "hour", true);
+        let minDiff = now.diff(defaultComment[i].date, "minute", true);
+        let secDiff = now.diff(defaultComment[i].date, "second", true);
 
-            if (hrDiff >= 1) {
-                defaultComment[i].showDate = hrDiff + " hours ago";
-            } else if (minDiff >= 1) {
-                defaultComment[i].showDate = minDiff + " minutes ago";
-            } else {
-                defaultComment[i].showDate = secDiff + " seconds ago";
-            }
+        if (monthDiff >= 12.5) {
+            defaultComment[i].show = defaultComment[i].date.format("MM/DD/YYYY");
+        } else if (monthDiff >= 11.5) {
+            defaultComment[i].show = "a year ago";
+        } else if (monthDiff >= 1.5) {
+            defaultComment[i].show = Math.round(monthDiff) + " months ago";
+        } else if (dayDiff >= 29.5) {
+            defaultComment[i].show = "a month ago";
+        } else if (dayDiff >= 1.5) {
+            defaultComment[i].show = Math.round(dayDiff) + " days ago";
+        } else if (hrDiff >= 23.5) {
+            defaultComment[i].show = "a day ago";
+        } else if (hrDiff >= 1.5) {
+            defaultComment[i].show = Math.round(hrDiff) + " hours ago";
+        } else if (minDiff >= 59.5) {
+            defaultComment[i].show = "a hour ago";
+        } else if (minDiff >= 1.5) {
+            defaultComment[i].show = Math.round(minDiff) + " minutes ago";
+        } else if (secDiff >= 59.5) {
+            defaultComment[i].show = "a minute ago";
         } else {
-            defaultComment[i].showDate = defaultComment[i].date;
+            defaultComment[i].show = Math.round(secDiff) + " seconds ago";
         }
     }
 }
@@ -115,24 +116,24 @@ const dynamicDate = () => {
 const submitHandle = (event) => {
     event.preventDefault();
     if (!submitStatus) return;
-    getDate();
 
     const formData = new FormData(form);
     let name = formData.get("name");
     let comment = formData.get("comment");
+    let now = dayjs();
+
     let newComment = {
         photo: true,
         name: name,
-        date: today,
-        time: time,
-        showDate: "",
+        date: now,
+        show: "",
         comment: comment
     };
 
     defaultComment.unshift(newComment);
     event.target.reset();
-    dynamicDate();
-    refreshComments(commentsDiv);
+    dynamicDate(now);
+    refreshComments();
 }
 
 const formCheck = (event) => {
@@ -169,6 +170,7 @@ const formCheck = (event) => {
     }
 }
 
+dynamicDate(dayjs());
 refreshComments();
 form.addEventListener("keyup", formCheck);
 form.addEventListener("submit", submitHandle);
